1ba8c15263abb6d5db931542ce450da723779178
arderitoe <eduardus.arderito@dana.id>
2020-04-27 16:25:20 +0700

    Initial Commit : DanaDemo.

---
+++ src/test/java/Employee.java
@@ -0,0 +1,60 @@
+import demo.EmployeeRequest;
+import demo.EmployeeResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Employee {
+    @Test
+    public void getEmployee(){
+        Response response = RestAssured
+                .given()
+                .baseUri("http://dummy.restapiexample.com")
+                .basePath("/api")
+                .log()
+                .all()
+                .header("Content-type", "application/json")
+                .header("Accept","*/*")
+                .get("/v1/employees");
+
+        //response.getBody().prettyPrint();
+        //System.out.println(response.getStatusCode());
+
+        //Assert.assertEquals(200, response.getStatusCode()); //code
+        //Assert.assertThat("Lama cui", response.getTime(), Matchers.lessThan(3000L)); //time
+        //Assert.assertEquals("success", response.path("status")); //body
+        //Assert.assertEquals("Tiger Nixon", response.path("data[0].employee_name"));
+
+        EmployeeResponse employeeResponse = response.as(EmployeeResponse.class);
+        System.out.println(employeeResponse.getStatus());
+
+    }
+
+    @Test
+    public void createEmployee(){
+        //String requestBody = "{\n" +
+          //      "  \"name\": \"Dana\",\n" +
+            //    "  \"salary\": \"123\",\n" +
+              //  "  \"age\": \"23\"\n" +
+                //"}";
+        EmployeeRequest employeeRequest = new EmployeeRequest();
+        employeeRequest.setName("Vans");
+        employeeRequest.setAge("23");
+        employeeRequest.setSalary("20000000");
+        Response response = RestAssured
+                .given()
+                .baseUri("http://dummy.restapiexample.com")
+                .basePath("/api")
+                .log()
+                .all()
+                .header("Content-type", "application/json")
+                .header("Accept","*/*")
+                .body(employeeRequest)
+                .get("/v1/create");
+                //.body(requestBody)
+        response.getBody().prettyPrint();
+
+    }
+}
